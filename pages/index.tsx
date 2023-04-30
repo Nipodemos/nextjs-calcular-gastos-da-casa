a7fc37e48aef700d944090605fa709af2349c799
Nipodemos <nipodemos13@gmail.com>
2023-04-30 16:13:13 -0300

    adiciona tipagem para o getserversideprops

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,38 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+type pessoasType = {
+  nome: string;
+  salario: number;
+  alimentacao: number;
+  inss: number;
+}
+
+type despesasType = {
+  id: number;
+  valor: number;
+  descricao: string;
+}
+
+type jsonBinType = {
+  pessoas: Array<pessoasType>;
+  despesas: Array<despesasType>;
+}
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const res = await fetch('https://json.extendsclass.com/bin/83eeef7011ba')
+//   const jsonBin: jsonBinType = await res.json()
+//   return {
+//     props: {
+//       jsonBin
+//     }
+//   }
+// }
+
 const arrayDespesas = [
-  { id: 1, valor: 400, descricao: 'pra comida' },
+  { id: 1, valor: 500, descricao: 'pra comida' },
   { id: 2, valor: 400, descricao: 'pra carne' },
-  { id: 3, valor: 520, descricao: 'pra semana' },
+  { id: 3, valor: 640, descricao: 'pra semana' },
   { id: 4, valor: 100, descricao: 'pra conta de internet' },
   { id: 5, valor: 200, descricao: 'cemig' },
   { id: 6, valor: 130, descricao: 'pra copasa' },
