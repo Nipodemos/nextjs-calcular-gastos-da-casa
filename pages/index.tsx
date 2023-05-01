8532f897940da2f0b4b57305973c542e803ffd5f
Nipodemos <nipodemos13@gmail.com>
2023-05-01 18:44:19 -0300

    usando jsonstorage pra pegar os dados

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import MostrarPessoas from '../components/mostrar_pessoas'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { GetServerSideProps } from 'next'
 
 type pessoasType = {
   nome: string;
@@ -24,16 +25,23 @@ type jsonBinType = {
   despesas: Array<despesasType>;
 }
 
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   const res = await fetch('https://json.extendsclass.com/bin/83eeef7011ba')
-//   const jsonBin: jsonBinType = await res.json()
-//   return {
-//     props: {
-//       jsonBin
-//     }
-//   }
-// }
-
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch('https://json.extendsclass.com/bin/83eeef7011ba', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Security-key': '33561820'
+    }
+  })
+  const jsonBin: jsonBinType = await res.json()
+  console.log('jsonBin :>> ', jsonBin);
+  return {
+    props: {
+      jsonBin
+    }
+  }
+}
+/*
 const arrayDespesas = [
   { id: 1, valor: 500, descricao: 'pra comida' },
   { id: 2, valor: 400, descricao: 'pra carne' },
@@ -62,17 +70,21 @@ const arrayPessoas = [
     inss: 0.09,
   }
 ]
+*/
 
-export default function Home() {
-  const [pessoas, setPessoas] = useState(arrayPessoas);
-  const [despesas, setDespesas] = useState(arrayDespesas);
+export default function Home({ jsonBin }: { jsonBin: jsonBinType }) {
+  console.log('jsonBin :>> ', jsonBin);
+  const [pessoas, setPessoas] = useState(jsonBin.pessoas);
+  const [despesas, setDespesas] = useState(jsonBin.despesas);
+  if (!jsonBin.pessoas) {
+    return <div>Tem algo errado</div>
+  }
   return (
     <Container fluid >
       <Row >
         <Col sm={6}><MostrarDespesas despesas={despesas} setDespesas={setDespesas} /></Col>
         <Col sm={3}><MostrarDivisao despesas={despesas} pessoas={pessoas} /></Col>
         <Col sm={3}><MostrarPessoas pessoas={pessoas} setPessoas={setPessoas} /></Col>
-
       </Row>
     </Container>
   )
