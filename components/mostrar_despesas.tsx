0dbce955493d441b10fb8ff68ef3bcb4f253a86c
Nipodemos <nipodemos13@gmail.com>
2023-05-01 18:44:54 -0300

    continuação das mudanças da tela principal

--- components/mostrar_despesas.tsx
+++ components/mostrar_despesas.tsx
@@ -1,5 +1,5 @@
-import { ChangeEvent, useState } from "react";
-import { Button, Col, Modal, Row, Table } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Col, Form, Modal, Row, Table } from "react-bootstrap";
 
 interface propsType {
   despesas: Array<despesasType>;
@@ -12,40 +12,55 @@ type despesasType = {
   descricao: string;
 }
 
-export default function MostrarDespesas({ despesas, setDespesas }: propsType) {
+type FormDataType = {
+  id: number | null;
+  valor: number;
+  descricao: string;
+}
 
+export default function MostrarDespesas({ despesas, setDespesas }: propsType) {
   const [show, setShow] = useState(false);
+  const [formData, setFormData] = useState<FormDataType>({
+    id: null,
+    valor: 0,
+    descricao: ''
+  });
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  const handleValueChange = (e: ChangeEvent<HTMLInputElement>, id: number) => {
-    const newDespesas = despesas.map((despesa) => {
-      if (despesa.id === id) {
-        return { ...despesa, valor: Number(e.target.value) };
+  const handleShow = (id: number | null) => {
+    if (id) {
+      const despesa = despesas.find((despesa) => despesa.id === id);
+      if (!despesa) {
+        throw new Error("Despesa não encontrada");
       }
-      return despesa;
-    });
-    setDespesas(newDespesas);
+      setFormData({ id: despesa.id, valor: despesa.valor, descricao: despesa.descricao })
+    } else {
+      setFormData({ id: null, valor: 0, descricao: '' })
+    }
+    setShow(true)
   };
+  const handleSave = () => {
+    if (formData.id === null) {
+      setDespesas([...despesas, { ...formData, id: despesas.length + 1, }])
+    } else {
+      setDespesas(despesas.map((despesa) => {
+        if (despesa.id === formData.id) {
+          return { ...despesa, ...formData }
+        }
+        return despesa
+      }))
+    }
+    setShow(false)
+  }
+
   const formatacao = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
-
-  const handleDescriptionChange = (e: ChangeEvent<HTMLInputElement>, id: number) => {
-    const newDespesas = despesas.map((despesa) => {
-      if (despesa.id === id) {
-        return { ...despesa, descricao: e.target.value };
-      }
-      return despesa;
-    });
-    setDespesas(newDespesas);
-  };
 
   return (
     <>
       <Row >
         <Col>
           <h1>Despesas</h1>
-          <Button variant="success" style={{ marginBottom: '8px' }} onClick={() => setDespesas([...despesas, { id: despesas.length + 1, valor: 0, descricao: '' }])}>Adicionar Despesa</Button>
+          <Button variant="success" style={{ marginBottom: '8px' }} onClick={() => handleShow(null)}>Adicionar Despesa</Button>
           <Table bordered>
             <thead>
               <tr>
@@ -60,7 +75,7 @@ export default function MostrarDespesas({ despesas, setDespesas }: propsType) {
                   <td>{formatacao.format(valor)}</td>
                   <td>{descricao}</td>
                   <td>
-                    <Button style={{ marginRight: '8px' }} onClick={handleShow} >Editar</Button>
+                    <Button style={{ marginRight: '8px' }} onClick={() => handleShow(id)} >Editar</Button>
                     <Button variant="danger" type="button" onClick={() => setDespesas(despesas.filter((despesaAtual) => despesaAtual.id !== id))}>Remover</Button>
                   </td>
                 </tr>
@@ -71,15 +86,29 @@ export default function MostrarDespesas({ despesas, setDespesas }: propsType) {
       </Row>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>{formData.id === null ? "Nova Despesa" : "Alterar Despesa"}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you&apos;re reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <Row>
+            <Form>
+              <Form.Group controlId="formValor">
+                <Form.Label>Valor</Form.Label>
+                <Form.Control type="number" placeholder="Valor" value={formData.valor} onChange={(e) => setFormData({ ...formData, valor: Number(e.target.value) })} />
+              </Form.Group>
+
+              <Form.Group controlId="formDescricao">
+                <Form.Label>Descrição</Form.Label>
+                <Form.Control type="text" placeholder="Descrição" value={formData.descricao} onChange={(e) => setFormData({ ...formData, descricao: e.target.value })} />
+              </Form.Group>
+            </Form>
+          </Row>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
-            Close
+            Fechar
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+          <Button variant="primary" onClick={handleSave}>
+            Gravar
           </Button>
         </Modal.Footer>
       </Modal>
