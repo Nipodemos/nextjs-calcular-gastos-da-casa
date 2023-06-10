cf653d1d6f4ac5678a026c97823d21f0a66f425d
Nipodemos <nipodemos13@gmail.com>
2023-06-09 22:39:12 -0300

    usando nova store derivada

--- components/mostrar_divisao.tsx
+++ components/mostrar_divisao.tsx
@@ -1,55 +1,23 @@
 import { use, useEffect, useState } from "react"
 import { mainStore } from "../stores/pessoa_e_despesa";
 import { Card, ListGroup } from "react-bootstrap";
-
-type valorPorPessoa = {
-  nome: string;
-  valor: number;
-  salario: number;
-}
+import { useValorPorPessoaStore } from "../stores/valor_por_pessoa";
 
 export default function MostrarDivisao() {
-  const despesas = mainStore((state) => state.despesas)
-  const pessoas = mainStore((state) => state.pessoas)
-  const [valorPorPessoa, setValorPorPessoa] = useState<Array<valorPorPessoa>>([]);
-
-  useEffect(() => {
-
-    const totalDespesas = despesas.reduce((acc, despesa) => {
-      return acc + despesa.valor
-    }, 0)
-
-    const totalReceitas = pessoas.reduce((acc, pessoa) => {
-      return acc + pessoa.salario - (pessoa.salario * pessoa.inss - 19.80) - (pessoa.salario * 0.06) + pessoa.alimentacao
-    }, 0)
-
-
-    setValorPorPessoa(pessoas.map(pessoa => {
-      const receitaPessoa = pessoa.salario - (pessoa.salario * pessoa.inss - 19.80) - (pessoa.salario * 0.06) + pessoa.alimentacao
-      const porcentagem = receitaPessoa / totalReceitas
-      console.log('\n\n\n---\npessoa.nome :>> ', pessoa.nome);
-      console.log('totalDespesas :>> ', totalDespesas);
-      console.log('totalReceitas :>> ', totalReceitas);
-      console.log('receitaPessoa :>> ', receitaPessoa);
-      console.log('porcentagem :>> ', porcentagem.toFixed(2));
-      const valor = Number(totalDespesas * porcentagem)
-      console.log('valor :>> ', valor);
-      return { nome: pessoa.nome, valor, salario: pessoa.salario, receita: receitaPessoa }
-    }))
-  }, [despesas, pessoas])
+  const valoresPorPessoa = useValorPorPessoaStore((state) => state);
 
 
   return (
     <>
       <h1 className="mb-5">Divisão</h1>
-      {valorPorPessoa.map(pessoa =>
-        <Card className="mb-3" key={pessoa.nome} style={{ width: '15rem' }}>
+      {valoresPorPessoa.map(({ nomePessoa, valor, valorQueSobra }) =>
+        <Card className="mb-3" key={nomePessoa} style={{ width: '15rem' }}>
           <Card.Body>
-            <Card.Title>{pessoa.nome}</Card.Title>
+            <Card.Title>{nomePessoa}</Card.Title>
           </Card.Body>
           <ListGroup className="list-group-flush">
-            <ListGroup.Item>Valor: {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(Number(pessoa.valor.toFixed(0)))}</ListGroup.Item>
-            <ListGroup.Item>Sobra: {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(Number((pessoa.salario - pessoa.valor).toFixed(0)))}</ListGroup.Item>
+            <ListGroup.Item>Valor: {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(Number(valor.toFixed(0)))}</ListGroup.Item>
+            <ListGroup.Item>Sobra: {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(Number(valorQueSobra.toFixed(0)))}</ListGroup.Item>
           </ListGroup>
 
         </Card>
--- components/mostrar_pessoas.tsx
+++ components/mostrar_pessoas.tsx
@@ -1,17 +1,24 @@
 import { Card, ListGroup } from "react-bootstrap";
 import { mainStore } from "../stores/pessoa_e_despesa";
+import { useStore } from "zustand";
+import { useValorPorPessoaStore } from "../stores/valor_por_pessoa";
+
 
 
 export default function MostrarPessoas() {
   const pessoas = mainStore((state) => state.pessoas)
+  const valorPorPessoa = useValorPorPessoaStore((state) => state);
   return (
     <div >
 
       <h1 className="mb-5" >Salários</h1>
-      {pessoas.map(({ nome, salario, alimentacao, inss }) => {
-        const inssValor = (salario * inss - 19.80);
-        const passagemValor = (salario * 0.06);
-        const total = salario - inssValor - passagemValor + alimentacao
+      {pessoas.map(({ nome, salario, alimentacao, inssPorcentagem }) => {
+        const valores = valorPorPessoa.find((valor) => valor.nomePessoa === nome);
+        if (valores === undefined) {
+          return <></>
+        }
+        const { inssValor, passagemValor, salarioLiquido } = valores;
+
         const formatacao = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
         return (
           <Card className="mb-3" key={nome} style={{ width: '15rem' }}>
@@ -21,9 +28,9 @@ export default function MostrarPessoas() {
             <ListGroup className="list-group-flush">
               <ListGroup.Item>Salário Bruto: {formatacao.format(salario)}</ListGroup.Item>
               <ListGroup.Item>Alimentação: {formatacao.format(alimentacao)}</ListGroup.Item>
-              <ListGroup.Item>INSS: {formatacao.format(inssValor * -1)} ({inss * 100}%)</ListGroup.Item>
+              <ListGroup.Item>INSS: {formatacao.format(inssValor * -1)} ({inssPorcentagem * 100}%)</ListGroup.Item>
               <ListGroup.Item>Passagem: {formatacao.format(passagemValor * -1)} (6%)</ListGroup.Item>
-              <ListGroup.Item>Salário líquido: {formatacao.format(total)}</ListGroup.Item>
+              <ListGroup.Item>Salário líquido: {formatacao.format(salarioLiquido)}</ListGroup.Item>
             </ListGroup>
           </Card>
         )
