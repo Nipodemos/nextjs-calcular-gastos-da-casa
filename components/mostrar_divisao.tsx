032a7f3cf8955099dddc38690202aa8fcce8a3bb
Nipodemos <nipodemos13@gmail.com>
2024-02-18 15:04:41 -0300

    mostrando porcentagem que cada um ta dando

--- components/mostrar_divisao.tsx
+++ components/mostrar_divisao.tsx
@@ -3,19 +3,24 @@ import { useValorPorPessoaStore } from "../stores/valor_por_pessoa";
 
 export default function MostrarDivisao() {
   const valoresPorPessoa = useValorPorPessoaStore((state) => state);
-
+  const formatter = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
 
   return (
     <>
       <h1 className="mb-5">Divisão</h1>
-      {valoresPorPessoa.map(({ nomePessoa, valor, valorQueSobra }) =>
+      {valoresPorPessoa.map(({ nomePessoa, valor, valorQueSobra, porcentagem }) =>
         <Card className="mb-3" key={nomePessoa} style={{ width: '15rem' }}>
           <Card.Body>
             <Card.Title>{nomePessoa}</Card.Title>
           </Card.Body>
           <ListGroup className="list-group-flush">
-            <ListGroup.Item>Valor: {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(Number(valor.toFixed(0)))}</ListGroup.Item>
-            <ListGroup.Item>Sobra: {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(Number(valorQueSobra.toFixed(0)))}</ListGroup.Item>
+            <ListGroup.Item>
+              Valor: {formatter.format(Number(valor.toFixed(0)))}
+              {' '} ({(porcentagem * 100).toFixed(0)}%)
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Sobra: {formatter.format(Number(valorQueSobra.toFixed(0)))}
+            </ListGroup.Item>
           </ListGroup>
 
         </Card>
