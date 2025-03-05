5ddcd183a2c8396381f4892676097e0b28ba3f31
Nipodemos <nipodemos13@gmail.com>
2025-03-04 21:14:37 -0300

    corrige importação

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from 'react'
-import MostrarDespesas from '../components/mostrar_despesas'
-import MostrarDivisao from '../components/mostrar_divisao'
-import MostrarPessoas from '../components/mostrar_pessoas'
+import MostrarDespesas from '../../components/mostrar_despesas'
+import MostrarDivisao from '../../components/mostrar_divisao'
+import MostrarPessoas from '../../components/mostrar_pessoas'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
-import { IPessoa, IDespesa, mainStore } from '../stores/pessoa_e_despesa'
-import prisma from '../prisma/db'
+import { IPessoa, IDespesa, mainStore } from '../../stores/pessoa_e_despesa'
+import prisma from '../../prisma/db'
 
 export type jsonBinType = {
   pessoas: Array<IPessoa>;
